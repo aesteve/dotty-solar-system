8383d2371bdcc5f0c6c98c419fb9188ccea8af7c
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2020-01-23 19:38:34 +0100

    enum with attributes

---
+++ src/test/scala/com.github.aesteve.dotty.examples.solarsystem/TestEnums.scala
@@ -0,0 +1,21 @@
+package com.github.aesteve.dotty.examples.solarsystem
+
+import org.junit._
+import org.junit.Assert._
+
+class TestPlanets
+
+  @Test
+  def weShouldBeSomewhere(): Unit = 
+    assertNotNull(ScalaPlanet.Earth)
+    assertNotNull(DottyPlanet.Earth)
+
+  @Test 
+  def testPlanetNumber(): Unit =
+    assertEquals(8, ScalaPlanet.values.size)
+    assertEquals(8, DottyPlanet.values.size)
+  
+  @Test
+  def jupiterIsTheHeaviestPlanet(): Unit = 
+    assertEquals(ScalaPlanet.Jupiter, ScalaPlanet.values.maxBy(_.mass))
+    assertEquals(DottyPlanet.Jupiter, DottyPlanet.values.maxBy(_.mass))

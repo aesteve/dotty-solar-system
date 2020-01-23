f6a341d18882d2fe001205b3753393759c12daa3
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2020-01-23 20:29:56 +0100

    add an opaque type

---
+++ src/main/scala/com/github/aesteve/dotty/examples/solarsystem/PostiveDouble.scala
@@ -0,0 +1,13 @@
+package com.github.aesteve.dotty.examples.solarsystem
+
+/*
+ * We're defining PositiveDouble type, that can only be constructed safely, but completely acts as a Double
+ * note the `<:` bound, that allows us to add PositiveDouble with a Double, order PositiveDouble, etc.
+ * there are tradeoffs though, like: (PositiveDouble.safe(2.0).get - 3.0d).getClass == double
+ * in fact, any arithmetic operation will "lose" the PositiveDouble type
+ */ 
+opaque type PositiveDouble <: Double = Double
+object PositiveDouble
+  def safe(double: Double): Option[PositiveDouble] =
+    if (double > 0.0d) then Some(double)
+    else None

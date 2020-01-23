17c99944daa5b15a5725e224ba945bbc5de760fc
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2020-01-23 22:24:59 +0100

    add scala 2 version in comment

--- src/main/scala/com/github/aesteve/dotty/examples/solarsystem/Constants.scala
+++ src/main/scala/com/github/aesteve/dotty/examples/solarsystem/Constants.scala
@@ -1,3 +1,10 @@
 package com.github.aesteve.dotty.examples.solarsystem
 
 val G: PositiveDouble = PositiveDouble.safe(6.67300E-11).get
+
+/*
+Scala 2 version for ref.
+package object Constants {
+  val G: PositiveDouble = PositiveDouble.safe(6.67300E-11).get
+}
+*/

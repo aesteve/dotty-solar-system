09a5defc2ec859867c87690e68b6f3bb4212e33e
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2020-01-23 20:28:51 +0100

    add an opaque type

--- src/main/scala/com/github/aesteve/dotty/examples/solarsystem/DottyPlanet.scala
+++ src/main/scala/com/github/aesteve/dotty/examples/solarsystem/DottyPlanet.scala
@@ -1,12 +1,14 @@
 package com.github.aesteve.dotty.examples.solarsystem
 
-enum DottyPlanet(val mass: Double, val radius: Double)
-  case Mercury extends DottyPlanet(3.303e+23, 2.4397e6)
-  case Venus   extends DottyPlanet(4.869e+24, 6.0518e6)
-  case Earth   extends DottyPlanet(5.976e+24, 6.37814e6)
-  case Mars    extends DottyPlanet(6.421e+23, 3.3972e6)
-  case Jupiter extends DottyPlanet(1.9e+27, 7.1492e7)
-  case Saturn  extends DottyPlanet(5.688e+26, 6.0268e7)
-  case Uranus  extends DottyPlanet(8.686e+25, 2.5559e7)
-  case Neptune extends DottyPlanet(1.024e+26, 2.4746e7)
+private def fromPos(double: Double): PositiveDouble = 
+  PositiveDouble.safe(double).get
 
+enum DottyPlanet(val mass: PositiveDouble, val radius: PositiveDouble)
+  case Mercury extends DottyPlanet(fromPos(3.303e+23), fromPos(2.4397e6))
+  case Venus   extends DottyPlanet(fromPos(4.869e+24), fromPos(6.0518e6))
+  case Earth   extends DottyPlanet(fromPos(5.976e+24), fromPos(6.37814e6))
+  case Mars    extends DottyPlanet(fromPos(6.421e+23), fromPos(3.3972e6))
+  case Jupiter extends DottyPlanet(fromPos(1.9e+27), fromPos(7.1492e7))
+  case Saturn  extends DottyPlanet(fromPos(5.688e+26), fromPos(6.0268e7))
+  case Uranus  extends DottyPlanet(fromPos(8.686e+25), fromPos(2.5559e7))
+  case Neptune extends DottyPlanet(fromPos(1.024e+26), fromPos(2.4746e7))
--- src/test/scala/com.github.aesteve.dotty.examples.solarsystem/TestEnums.scala
+++ src/test/scala/com.github.aesteve.dotty.examples.solarsystem/TestEnums.scala
@@ -7,15 +7,12 @@ class TestPlanets
 
   @Test
   def weShouldBeSomewhere(): Unit = 
-    assertNotNull(ScalaPlanet.Earth)
     assertNotNull(DottyPlanet.Earth)
 
   @Test 
   def testPlanetNumber(): Unit =
-    assertEquals(8, ScalaPlanet.values.size)
     assertEquals(8, DottyPlanet.values.size)
   
   @Test
   def jupiterIsTheHeaviestPlanet(): Unit = 
-    assertEquals(ScalaPlanet.Jupiter, ScalaPlanet.values.maxBy(_.mass))
     assertEquals(DottyPlanet.Jupiter, DottyPlanet.values.maxBy(_.mass))
